cc836efa92ae4e89014b31bf16f4ce480997ac05
Andrea Restrepo <andrearestrepox@gmail.com>
2022-09-15 23:54:25 -0400

    fixed home page

--- src/components/IsPrivateAndNoProfile.js
+++ src/components/IsPrivateAndNoProfile.js
@@ -5,16 +5,16 @@ import { Navigate } from "react-router-dom";
 function IsPrivateAndNoProfile({ children }) {
   const { isLoggedIn, isLoading, user } = useContext(AuthContext);
 
-  // If the authentication is still loading ⏳
+  // If the authentication is still loading 
   if (isLoading) return <p>Loading ...</p>;
 
   if (!isLoggedIn) {
-  // If the user is not logged in ❌
+  // If the user is not logged in 
     return <Navigate to="/login" />;
   } else if(isLoggedIn && user && user.profileId) {
     return <Navigate to="/users" />;
   } else {
-  // If the user is logged in, allow to see the page ✅
+  // If the user is logged in, allow to see the page 
     return children;
   }
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,30 +1,27 @@
-import jengarner from '../components/jengarner.webp'
-import singlemom1 from '../components/istockphoto-1035146258-612x612.jpg'
-import singlemom2 from '../components/single mom3.jpg'
-import typingimg from '../components/typingimg.jpg'
+
+import display from '../assets/img/Untitled design (1).png'
+import { Link } from "react-router-dom";
 
 function HomePage() {
     return (
         <div>
-        <div className='DivBlock'>
-            <h3 className='TextDiv'>
+        {/* <div className='DivBlock'> */}
+            {/* <h3 className='TextDiv'>
             This is for her. The one who spends countless nights rocking her baby to sleep. The one who wakes up every two hours to feed her new-born. The one who checks for monsters under the bed and behind closet doors. The one who forgets to eat because there is too much to do. This is for you. The you who goes on hot-girl walks after nap time. The you who enjoys a good book over an even better coffee. The you who tells the craziest stories at brunch with the girls. This is for you and for her; for the single moms in South Florida who desire to see a strong, loving community of women making it happen. We know it takes a village, why not choose yours?
            
-            </h3>
+            </h3> */}
         
+            {/* </div> */}
+            <div className='opentxt'>
+              <h1 className='opentitle'>Join Mom Meet Up to <br/>meet moms like you</h1>
+              <p className='openpar'>Connect with moms who are at a similar stage in life and build your tribe - <br/>we know it takes a village, why not choose yours? </p>
             </div>
-      
-        <div class="row">
-  <div class="column">
-  <img className='HomeImg1' src={jengarner}/>
-  </div>
-  <div class="column">
-  <img className='HomeImg1' src={singlemom1}/>
-  </div>
-  <div class="column">
-    <img className="HomeImg1" src={singlemom2}/>
-  </div>
-</div>
+            <div>
+              <img className='openimg' src={display}/>
+            </div>
+            <div>
+            <button className="signBtn" type="submit"> <Link className="signbtn" to={"/signup"}>Join</Link></button>
+            </div>
         
 
 
